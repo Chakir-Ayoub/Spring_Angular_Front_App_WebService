4183ec30fe5915491d07c11c6fed2ac2e8b1d68c
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2023-05-03 01:36:15 +0100

    Create Components & Services & Models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddAddressComponent } from './component/add-address/add-address.component';
+import { EditAddressComponent } from './component/edit-address/edit-address.component';
+import { ListAddressComponent } from './component/list-address/list-address.component';
+import { NavbarComponent } from './component/partials/navbar/navbar.component';
+import { PagesNotFoundComponent } from './component/partials/pages-not-found/pages-not-found.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAddressComponent,
+    EditAddressComponent,
+    ListAddressComponent,
+    NavbarComponent,
+    PagesNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/list-address/list-address.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-address',
+  templateUrl: './list-address.component.html',
+  styleUrls: ['./list-address.component.css']
+})
+export class ListAddressComponent {
+
+}
